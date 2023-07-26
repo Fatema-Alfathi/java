fb506e0d3ba48d8f8240b9bc53806bb2b7de3eed
Fatema-Alfathi <Fatemaalfathia@gmail.com>
2023-07-26 17:03:03 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        //Even number by for
+//        for(int a = 1; a <= 100; a++) {
+//            if (a% 2 == 0) {
+//                System.out.println(a + " even");
+//            } else {
+//                System.out.println(a + " it is not even");
+//            }
+//        }
+
+
+        //odd number by while
+//        int y=1;
+//        while(y<=100){
+//            if (y%2!=0){
+//                System.out.println( y+" It is Odd");
+//
+//            }else{
+//                System.out.println(y);
+//            }
+//            y++;
+//        }
+
+
+        //Prime numbers by do while
+//        int i = 1;
+//        do {
+//            if (i!=1 && i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0
+//            || i==2 || i==3 || i==4 || i==5 || i==7){
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//        while (i <=100);
+
+
+        //Use the Array with for Loop
+//        String[] question = {"1+2=", "1-1=", "3+4=", "7==7?"};
+//        String[] answer = {"3", "0", "7", "true"};
+//        Scanner read = new Scanner(System.in); // to input
+//
+//        System.out.println("Enter your name: ");// massage to input name
+//        String name= read.next(); //receives a value in program
+//
+//        System.out.println("Enter number of question"); // massage to input number of question
+//        int q = read.nextInt();
+//
+//        int count=0; // first value to calculate score
+//
+//       for(int i=0;i<q;i++){ //Each time the program displays a question (عدد الاسالة هو العدد المدخل من قبل المستخدم)
+//           System.out.println(question[i]); // (i=عداد لأسألة و الأجوبة)يطبع سوال من ليست الاسألة على حسب قيمة i
+//           String userAns= read.next(); //يسمح لمستخدم بأدخال اجابة و حفظها في متغير اسمه userAns
+//
+//           if (userAns.equals(answer[i])){ // هنا يساوي الاجابة المدخلة مع الجواب تبع i المحفوظة في array of question
+//               System.out.println("correct" ); //اذا كان الجواب صحيح
+//               count= count+1; //نضيف واحد لكل اجابة صحيحة
+//           }else{
+//               System.out.println("not correct");
+//           }
+//           System.out.println(" your score: " + count);
+//
+//           }
+//        System.out.println(" final score " + count + "out of "+ q);
+//
+//       }
+
+      
+
+
+    }
+}
+
