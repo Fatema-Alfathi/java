8ae6e184ce2edb150c61eb77cb5e98f80cbf41b6
Fatema-Alfathi <132199582+Fatema-Alfathi@users.noreply.github.com>
2023-08-07 13:57:05 +0400

    Add files via upload

---
+++ varible/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        variable
+         double w=1.1;
+       String w1="Hoor";
+        int w2=34;
+       boolean w3=true;
+        System.out.println(w3);
+        System.out.println(w2);
+       System.out.println(w1);
+       System.out.println(w);
+
+    }
+}
