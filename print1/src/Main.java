972c28ba8999a63f600e342688c557d7d7db8dbf
Fatema-Alfathi <132199582+Fatema-Alfathi@users.noreply.github.com>
2023-08-07 13:46:43 +0400

    Add files via upload

---
+++ print1/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //print Text
+        System.out.println("Hello");
+//print Number
+        System.out.println(342);
+        System.out.println(33.5);
+
+
+//print multi patron
+        System.out.println("     J        a         v         a");
+        System.out.println("    JJJ      aaa       vvv       aaa");
+        System.out.println("   JJJJJ    aaaaa     vvvvv     aaaaa");
+        System.out.println("                     vvvvvvv   aaaaaaa");
+        System.out.println("                              aaaaaaaaa");
+
+        System.out.println(" ---    ---    ---");
+        System.out.println("|   |  |   |  |   |");
+        System.out.println(" ---    ---    ---");
+        System.out.println("|   |  |   |  |   |");
+        System.out.println(" ---    ---    ---");
+        System.out.println("|   |  |   |  |   |");
+        System.out.println(" ---    ---    ---");
+
+
+        System.out.println("o o o m m m m m m m ");
+        System.out.println("o o o n n n n n n n");
+        System.out.println("o o o m m m m m m m ");
+        System.out.println("o o o a a a a a a a");
+        System.out.println("o o o a a a a a a a");
+        System.out.println("o o o n n n n n n n");
+
+    }
+}
